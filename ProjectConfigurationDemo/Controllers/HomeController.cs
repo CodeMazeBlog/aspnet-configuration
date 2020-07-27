bd149154f3208bbe8b0bd7d8c5e5eab1e19e41fd
Code Maze <codemazeblog@gmail.com>
2020-07-27 14:59:45 +0200

    Initial commit

---
+++ ProjectConfigurationDemo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ProjectConfigurationDemo.Models;
+
+namespace ProjectConfigurationDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IConfiguration _configuration;
+
+		public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		public IActionResult Index()
+		{
+			var logLevelConfiguration = new LoggingLevelConfiguration();
+			_configuration.Bind("Logging:LogLevel", logLevelConfiguration);
+
+			var homeModel = new HomeModel
+			{
+				DefaultLogLevel = logLevelConfiguration.Default
+			};
+
+			return View(homeModel);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
